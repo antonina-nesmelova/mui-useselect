694a47df4c5948994a6b8387a71b01855af4da67
Antonina Nesmelova <antonina.nesmelova@gmail.com>
2023-08-29 11:24:37 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import Demo from './Demo';
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <React.StrictMode>
+    <Demo />
+  </React.StrictMode>
+);
